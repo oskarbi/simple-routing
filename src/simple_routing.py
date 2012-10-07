9b7095ff51e0b6bf98a054c48d596bff08b7aa63
Oskarbi <oscar.teixe@gmail.com>
2012-10-08 00:08:41 +0200

    Add the class Router encapsulating the main functionality.

--- src/simple_routing.py
+++ src/simple_routing.py
@@ -8,23 +8,33 @@ import algorithm
 import csv_parser
 import models
 
+class Router(object):
+
+    def __init__(self):
+        self.graph = None
+
+    def load_graph(self):
+        self.graph = models.Graph()
+        stop_list = csv_parser.parse_stops()
+        for stop in stop_list:
+            self.graph.add_stop(stop)
+
+        trip_list = csv_parser.parse_stop_times()
+        for trip in trip_list:
+            self.graph.add_trip(trip, trip_list[trip])
+            for stop_id, next_stop_id in zip(trip_list[trip], trip_list[trip][1:]):
+                if next_stop_id not in self.graph.get_stop(stop_id).neighbors:
+                    self.graph.get_stop(stop_id).add_neighbor(next_stop_id)
+                    self.graph.get_stop(stop_id).add_trip(trip)
+
+    def run(self, origin, destination):
+        return algorithm.dijkstra(self.graph, origin, destination)
+
 def main():
-    """
-    Ejecuta un ejemplo
-    """
-    graph = models.Graph()
-    stop_list = csv_parser.parse_stops()
-    for stop in stop_list:
-        graph.add_stop(stop)
-
-    trip_list = csv_parser.parse_stop_times()
-    for trip in trip_list:
-        for stop, next_stop in zip(trip_list[trip], trip_list[trip][1:]):
-            if next_stop not in stop.neighbors:
-                graph.get_stop(stop).add_neighbor(next_stop)
-
-    # This is an example:
-    path, dist = algorithm.dijkstra(graph, "2317", "2327")
+    """Run an example."""
+    router = Router()
+    router.load_graph()
+    path, dist = router.run("2327", "2317")
     print path
     print dist
 
